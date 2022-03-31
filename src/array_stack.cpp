827291f30c0fae814533bc37778e64c832e107e3
Ilya <perk.1@mail.ru>
2022-03-31 23:29:44 +0300

    commit_31_03_2022

--- src/array_stack.cpp
+++ src/array_stack.cpp
@@ -11,6 +11,8 @@ namespace assignment {
     if (capacity <= 0) {
       throw std::invalid_argument("capacity is not positive");
     }
+    capacity_ = capacity;
+    data_ = new int[capacity]{0};
 
     // Write your code here ...
   }
@@ -20,42 +22,82 @@ namespace assignment {
   }
 
   void ArrayStack::Push(int value) {
-    // Write your code here ...
+    if (size_ == capacity_) {
+      Resize(capacity_ + kCapacityGrowthCoefficient);
+      data_[size_] = value;
+      size_++;
+      return;
+    }
+    data_[size_] = value;
+    size_++;
   }
 
+
   bool ArrayStack::Pop() {
+    if (size_ > 0){
+      int* newarr = new int[size_ - 1];
+      for (int i = 0; i < size_ - 1; i++) {
+        newarr[i] = data_[i];
+      }
+
+      delete[] data_;
+      data_ = newarr;
+      size_ = size_ - 1;
+      return true;
+
+    }
     // Write your code here ...
     return false;
   }
 
   void ArrayStack::Clear() {
+    int* newarr = new int[0];
+    delete[] data_;
+    data_ = newarr;
+    size_ = 0;
     // Write your code here ...
   }
 
   std::optional<int> ArrayStack::Peek() const {
+    if (size_ > 0){
+      return data_[size_- 1];
+
+    }
     // Write your code here ...
     return std::nullopt;
   }
 
   bool ArrayStack::IsEmpty() const {
-    // Write your code here ...
-    return false;
+    return size_ == 0;
   }
 
   int ArrayStack::size() const {
     // Write your code here ...
-    return 0;
+    return size_;
   }
 
   int ArrayStack::capacity() const {
     // Write your code here ...
-    return 0;
+    return capacity_;
   }
 
   bool ArrayStack::Resize(int new_capacity) {
     // Write your code here ...
+    if(new_capacity > capacity_){
+      int* arr = new int[new_capacity];
+      int cap = capacity_;
+      std::copy(data_, data_ + capacity_, arr);
+      delete[] data_;
+      data_ = arr;
+      capacity_ = new_capacity;
+      if (capacity_ > cap) {
+        return true;
+      }
+      return false;
+    }
     return false;
   }
+
 
   // ДЛЯ ТЕСТИРОВАНИЯ
   ArrayStack::ArrayStack(const std::vector<int>& values, int capacity) {
